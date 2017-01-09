4a4aec00d4b956d8adb26ad73c99d523fce5024a
Makiwilb <codeeblan@hotmail.com>
2017-01-09 18:02:02 +0800

    Project Initialized

---
+++ TDD_1_GroupTotal/TDD_1_GroupTotal/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_1_GroupTotal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
